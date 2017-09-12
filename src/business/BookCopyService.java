c44ad52a7c0947e9cabcb9aa8fad4c43ffafd132
AmosJindao <liusr@outlook.com>
2017-09-12 15:39:52 -0500

    add checkout book

--- src/business/BookCopyService.java
+++ src/business/BookCopyService.java
@@ -3,18 +3,23 @@ package business;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessFacade;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 public class BookCopyService{
 
     public void AddBookCopy(String isbn) throws LibrarySystemException {
-        if(isbn == null || isbn.trim().equals("")){
+        if (isEmpty(isbn)) {
             throw new LibrarySystemException("isbn can not be empty!");
         }
         DataAccess da = new DataAccessFacade();
 
         Book book = da.findBookByIsbn(isbn);
 
-        if(book == null){
-            throw new LibrarySystemException("The book whose isbn is "+isbn+" doesn't exist!");
+        if (book == null) {
+            throw new LibrarySystemException("The book whose isbn is " + isbn + " doesn't exist!");
         }
 
         book.addCopy();
@@ -22,9 +27,48 @@ public class BookCopyService{
         da.saveBook(book);
     }
 
-    public void checkoutBook(String memberId, String isbn) {
+    public void checkoutBook(String memberId, String isbn) throws LibrarySystemException {
+        if (isEmpty(memberId)) {
+            throw new LibrarySystemException("memberId can not be empty!");
+        }
+
+        if (isEmpty(isbn)) {
+            throw new LibrarySystemException("isbn can not be empty!");
+        }
+
         DataAccess da = new DataAccessFacade();
 
-        da.
+        LibraryMember member = da.findMemberById(memberId);
+        if (member == null) {
+            throw new LibrarySystemException("Member doesn't exist! memberId=" + memberId);
+        }
+
+        Book book = da.findBookByIsbn(isbn);
+        if (book == null) {
+            throw new LibrarySystemException("Book doesn't exist! isbn=" + isbn);
+        }
+
+        if (!book.isAvailable()) {
+            throw new LibrarySystemException("Book is not available! isbn=" + isbn);
+        }
+
+        BookCopy bookCopy = book.getNextAvailableCopy();
+
+//        bookCopy.changeAvailability();
+
+        Date dateOfCheckout = new Date();
+        Date dueDate = Date.from(LocalDateTime.from(dateOfCheckout.toInstant()).plus(book.getMaxCheckoutLength(), ChronoUnit.DAYS).toInstant(ZoneOffset.of("-06:00")));
+
+        new MemberService().checkout(bookCopy, new Date(), dueDate);
+
+        da.saveBook(book);
+    }
+
+    private boolean isEmpty(String content) {
+        if (content == null || content.trim().equals("")) {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/business/MemberService.java
+++ src/business/MemberService.java
@@ -1,5 +1,15 @@
 package business;
 
+import java.util.Date;
+
 public class MemberService{
 
+    public void checkout(BookCopy bookCopy, Date dateOfCheckout, Date dueDate)
+            throws LibrarySystemException{
+        //TODO
+        if(bookCopy == null || bookCopy.isAvailable()){
+            throw new LibrarySystemException("bookCopy is not available!");
+        }
+
+    }
 }
