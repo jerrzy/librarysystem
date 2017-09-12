a05d4a9ebe04271bc8502ca648bda1f0d0c2534b
AmosJindao <liusr@outlook.com>
2017-09-12 12:11:12 -0500

    add class diagram

--- src/business/Author.java
+++ src/business/Author.java
@@ -3,14 +3,21 @@ package business;
 import java.io.Serializable;
 
 final public class Author extends Person implements Serializable {
+	private String credentials;
 	private String bio;
+
+	public String getCredentials() {
+		return credentials;
+	}
+
 	public String getBio() {
 		return bio;
 	}
 	
 
-	public Author(String f, String l, String t, Address a, String bio) {
+	public Author(String f, String l, String t, Address a, String credentials, String bio) {
 		super(f, l, t, a);
+        this.credentials = credentials;
 		this.bio = bio;
 	}
 
