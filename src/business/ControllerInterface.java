62fe5d0748dd8508e07b3cb7476ebf74ba4da010
AmosJindao <liusr@outlook.com>
2017-09-12 13:32:03 -0500

    add member method

--- src/business/ControllerInterface.java
+++ src/business/ControllerInterface.java
@@ -8,7 +8,11 @@ import dataaccess.DataAccessFacade;
 
 public interface ControllerInterface {
 	public void login(String id, String password) throws LoginException;
+
+	void addMember();
+
 	public List<String> allMemberIds();
+
 	public List<String> allBookIds();
 	
 }
